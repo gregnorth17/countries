3c8477c62838d9458bbe9acec2d014bb00d16520
gregnorth17 <greg_north@hotmail.co.uk>
2022-07-08 15:47:58 -0500

    country page

--- src/components/countries/country-card/CountryCard.js
+++ src/components/countries/country-card/CountryCard.js
@@ -2,28 +2,26 @@ import "./CountryCard.css";
 
 const CountryCard = (props) => {
 	console.log(props);
-	// return props.countries.filter(countryName => {
-	// 	if(props.searchInput === "") {
-	// 		return countryName;
-	// 	} else if(
-	// 		countryName.name.common.toLowerCase()
-	// 		.includes(props.searchInput.toLowerCase())
-	// 		) {
-	// 			return countryName;
-	// 		}
-	// })
-	// .filter(countryRegion => {
-	// 	if(countryRegion.region.toLower)
-	// })
 	return props.countries.filter(countryName => {
 		if(props.inputs.country === "") {
 			return countryName;
-		} else if(countryName.name.common.toLowerCase()
-							.includes(props.inputs.country.toLowerCase())) {
+		} else if(
+							countryName.name.common.toLowerCase()
+							.includes(props.inputs.country.toLowerCase())
+							) {
 								return countryName;
 							}
 	})
-	
+	.filter(countryRegion => {
+		if(props.inputs.region === "") {
+			return countryRegion;
+		} else if(
+							countryRegion.region.toLowerCase()
+							=== props.inputs.region.toLowerCase()
+		) {
+			return countryRegion;
+		}
+	})
 	.map((country, index) => {
 		return (
 			<div key={index} className="country">
