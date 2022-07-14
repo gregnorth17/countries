febf14605d442fca8bcbc6d7e270344247d5c506
gregnorth17 <greg_north@hotmail.co.uk>
2022-07-14 15:34:57 -0500

    country page

--- src/components/CountryPage.js
+++ src/components/CountryPage.js
@@ -1,46 +1,58 @@
 import {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
+import {useParams, Link} from "react-router-dom";
 import "./CountryPage.css";
 
 const CountryPage = (props) => {
-	console.log(props)
+	// console.log(props)
 	const [country, setCountry] = useState([]);
 	const {countryName} = useParams();
-	useEffect(() => {
-		fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-			.then(response => response.json())
-			.then(data => setCountry(data));
+
+	useEffect( () => {
+		const fetchData = async () => {
+			const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+			const data = await response.json();
+			setCountry(data)
+		}
+		
+		fetchData()
+		.catch(console.error)
 	},[])
 
 	console.log(country);
+
+	// console.log(population, region, name)
 	try {
-		return (
-			<div className="country-page">
-				<div className="country-flag">
-	
+		return country.map(country => {
+			console.log(country);
+			
+			return (
+				<div className="country-page">
+					<div className="country-flag">
+						<img src={country.flags.png} alt="flag"/>
+					</div>
+					<div className="country-data">
+					<Link to={"/"}><button>{"<-Back"}</button></Link>
+						<h2 className="country-name">{country.name.common}</h2>
+						<div>
+							<p className="country-stats"><span className="country-stats-bold">Native Name: </span>{country.name.nativeName.cha.official}</p>
+							<p className="country-stats"><span className="country-stats-bold">Population: </span>{country.population}</p>
+							<p className="country-stats"><span className="country-stats-bold">Region: </span>{country.region}</p>
+							<p className="country-stats"><span className="country-stats-bold">Sub Region: </span>{country.subregion}</p>
+							<p className="country-stats"><span className="country-stats-bold">Capital: </span>{country.capital}</p>
+						</div>
+						<div>
+							<p className="country-stats"><span className="country-stats-bold">Top Level Domain: </span>{country.tld}</p>
+							<p className="country-stats"><span className="country-stats-bold">Currencies: </span>{country.currencies.USD.name} {country.currencies.USD.symbol}</p>
+							<p className="country-stats"><span className="country-stats-bold">Languages: </span>{country.languages.cha}, {country.languages.eng}, {country.languages.spa}</p>
+							
+						</div>
+						<div className="country-borders">
+							<p className="country-stats"><span className="country-stats-bold"></span></p>
+						</div>
+					</div>
 				</div>
-				<div className="country-data">
-				<button>{"<-Back"}</button>
-					<h2 className="country-name">{country[0].name.common}</h2>
-					<div>
-						<p className="country-stats"><span className="country-stats-bold">Native Name: </span>{country[0].name.nativeName.tur.common}</p>
-						<p className="country-stats"><span className="country-stats-bold">Population: </span></p>
-						<p className="country-stats"><span className="country-stats-bold">Region: </span></p>
-						<p className="country-stats"><span className="country-stats-bold">Sub Region: </span></p>
-						<p className="country-stats"><span className="country-stats-bold">Capital: </span></p>
-					</div>
-					<div>
-						<p className="country-stats"><span className="country-stats-bold">Top Level Domain: </span></p>
-						<p className="country-stats"><span className="country-stats-bold">Currencies: </span></p>
-						<p className="country-stats"><span className="country-stats-bold">Languages: </span></p>
-						
-					</div>
-					<div className="country-borders">
-						<p className="country-stats"><span className="country-stats-bold"></span></p>
-					</div>
-				</div>
-			</div>
-		)
+			)
+		})
 	} catch(error) {
 		console.log(error);
 	}
